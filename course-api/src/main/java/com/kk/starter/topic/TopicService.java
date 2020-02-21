86c99d55bdc47b3b2ab73aad9eb3cf661c0f6325
KARTHIK KUMAR <karthik_k_007@yahoo.co.in>
2020-02-21 10:52:43 +0530

    Add business service and controller

---
+++ course-api/src/main/java/com/kk/starter/topic/TopicService.java
@@ -0,0 +1,29 @@
+package com.kk.starter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring framework", "Spring framework description"),
+			new Topic("java", "core java", "core java description"),
+			new Topic("java script", "Java Script", "Java Script description")
+		));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+}
